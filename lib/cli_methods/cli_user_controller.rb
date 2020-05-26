c65e828f8d3cc2e6b880ffbda448ea6c81ee2650
JonTDean <JonathanTomDean@Gmail.com>
2020-05-26 17:15:51 -0400

    MVPUserEdit Created UserEdit

--- lib/cli_methods/cli_user_controller.rb
+++ lib/cli_methods/cli_user_controller.rb
@@ -42,13 +42,30 @@ class CLIUserController
     end
 
     # Update Account 
-    # def update_account_menu
-    
-    # end
+    def self.update_account_menu
+        property = CLI.prompts.select("What Setting would you like to update?", [:name, :password])
+        self.update_account_start(property)
+    end
 
-    # def update_account_start(property)
-    #     @@current_user.update()
-    # end
+    # Performs Edit of Table on user that is logged in
+    def self.update_account_start(property)
+        case property
+        when :password
+            change = CLI.prompts.ask("What do you want to change #{property} to?") do |q|        # Prompts for Password
+                q.required true                                                                  # Requires Special Properties defined at <q>
+                q.validate /^[0-9a-zA-Z]*$/                                                      # Performs comparison check, if true validate is true if false validate is false
+                q.messages[:valid?] = "Please use Letters(Uppercase or Lowercase) and numbers"   # Set custom <valid?> Property https://www.rubydoc.info/gems/tty-prompt/TTY%2FPrompt%2Emessages
+            end
+        when :name
+            change = CLI.prompts.ask("What do you want to change #{property} to?") do |q|        # Prompts for User Name
+                q.required true                                                                  # Requires Special Properties defined at <q>
+                q.validate /^[a-zA-Z]*$/                                                         # Performs comparison check, if true validate is true if false validate is false
+                q.messages[:valid?] = "Please use Letters(Uppercase or Lowercase)"               # Set custom <valid?> Property https://www.rubydoc.info/gems/tty-prompt/TTY%2FPrompt%2Emessages
+            end
+        end
+
+        @@current_user.update(property => change)
+    end
     
 end
 
