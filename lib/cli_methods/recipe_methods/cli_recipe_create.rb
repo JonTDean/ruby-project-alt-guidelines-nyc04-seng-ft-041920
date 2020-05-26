9d6653d117b673228f9e6e77b4b3ab4ea11e9b1c
github username <github email address>
2020-05-26 14:33:37 -0400

    getting access to user_id

--- lib/cli_methods/recipe_methods/cli_recipe_create.rb
+++ lib/cli_methods/recipe_methods/cli_recipe_create.rb
@@ -4,26 +4,24 @@ require_relative '../../models/cli'
 # require_relative '../../models/user'
 
 class RecipeCreation
-    @cli = CLI.new
     def self.ask_for_recipe_details
-        title = @cli.prompt.ask("What is the name of your recipe?")
+        title = CLI.prompts.ask("What is the name of your recipe?")
 
         # check all the recipes in the database
         # if that name already exists, ask for a new one
         while Recipe.find_by(title: title)
-            title = @cli.prompt.ask("Sorry, that name is already taken. Please type another name for your recipe:")
+            title = CLI.prompts.ask("Sorry, that name is already taken. Please type another name for your recipe:")
         end
   
-        category = @cli.prompt.select("Please choose a category", %w(Cake Cookies Pie Cheesecake Frosting Ice\ Cream\ or\ Frozen Pudding Other))
+        category = CLI.prompts.select("Please choose a category", %w(Cake Cookies Pie Cheesecake Frosting Ice\ Cream\ or\ Frozen Pudding Other))
 
-        #################need to grab user id
-        Recipe.create(user_id: CLIUserController.current_user?, title: title, category:category)
+        Recipe.create(user_id: CLIUserController.current_user?.id, title: title, category:category)
     end
 
     #action could be delete, update, view 
     def self.show_user_recipes(action)
-        elisheva = User.find(2)
-        title = @cli.prompt.select("Which recipe would you like to #{action}?", elisheva.user_recipe_titles)
+        current_user = CLIUserController.current_user?
+        title = CLI.prompts.select("Which recipe would you like to #{action}?", current_user.user_recipe_titles)
 
         recipe = Recipe.find_by(title: title)
         case action
@@ -36,8 +34,8 @@ class RecipeCreation
 
         when "update"
             #update recipe
-            item = @cli.prompt.select("Which item would you like to #{action}?", recipe.view_recipe)
-            item_part = @cli.prompt.select("Which part would you like to #{action}?", item.split(" "))
+            item = CLI.prompts.select("Which item would you like to #{action}?", recipe.view_recipe)
+            item_part = CLI.prompts.select("Which part would you like to #{action}?", item.split(" "))
             #actually update it
             
         else
