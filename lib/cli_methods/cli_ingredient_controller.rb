0e2a7a43fdf9b944cc20b4559f57bb52b02e01b6
JonTDean <JonathanTomDean@Gmail.com>
2020-05-26 14:45:54 -0400

    Removed CLIIngredients ADD + DELETE Ingredients

--- lib/cli_methods/cli_ingredient_controller.rb
+++ lib/cli_methods/cli_ingredient_controller.rb
@@ -14,7 +14,20 @@ class IngredientController
         0 # Not a Typo, used for return
     end
 
-    def self.add_ingredient(input)
-        Ingredient.create(name: input)
-    end
+    # def self.add_ingredient(input)
+    #     if !Ingredient.all.include?(input)
+    #         Ingredient.create(name: input)
+    #     end
+    # end
+
+    # def self.remove_ingredient(input)
+    #     ingredient_to_remove = Ingredient.find_by(name: input)
+    #     choice = CLI.prompts.select("Are you sure you want to delete this Ingredient?", ["Yes", "No"])
+    #     case choice 
+    #     when /Yes/
+    #         Ingredient.destroy(ingredient_to_remove.id)
+    #     when /No/
+    #         puts "Ingredient not Deleted."
+    #     end
+    # end
 end
